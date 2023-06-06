6b8e252fcdbc240ef38725b874e37184e2b2db55
P-Heckmann <paul.he@posteo.de>
2023-06-06 10:49:12 +0200

    c

--- src/data/functions.py
+++ src/data/functions.py
@@ -2,15 +2,17 @@ import pandas as pd
 import os
 
 
-def merge_textfiles(directory_path,name='merged.txt'):
-    """merges multiple txt files into one and saves it.
+def merge_textfiles(directory_path, name="merged.txt"):
+    """
+    Merges multiple text files into one and saves the merged content.
 
     Args:
-        directory_path (_type_): _description_
-        name (str, optional): _description_. Defaults to 'merged.txt'.
+        directory_path (str): The path to the directory containing the text files.
+        name (str, optional): The name of the merged file to be saved. Defaults to 'merged.txt'.
     """
     # Create an empty string to store the merged data
     merged_data = ""
+
     # Loop through all files in the directory
     for i, filename in enumerate(os.listdir(directory_path)):
         if filename.endswith(".txt"):
@@ -25,22 +27,38 @@ def merge_textfiles(directory_path,name='merged.txt'):
             # Add the contents to the merged_data string with a semicolon separator
             merged_data += "".join(file_contents).strip() + ";"
 
-    with open(
-        os.path.join(
-            rf"C:\Users\paulh\Desktop\Weather\data\merged\{name}"
-        ),
-        "w",
-    ) as file:
+    # Save the merged data to a file
+    with open(os.path.join(directory_path, name), "w") as file:
         file.write(merged_data)
 
 
 
+def yearlyAverageDataframe(directory_path, name="pickled"):
+    """
+    Loads a dataset from the specified directory 
+    path and performs operations to calculate the yearly average 
+    metric for each state. The resulting dataframe is transformed
+    from wide to long format and saved as a pickle file.
 
-def yearlyAverageDataframe(directory_path):
+    Args:
+        directory_path (str): The path to the directory containing the air temperature dataset.
+        name (str, optional): The name of the pickle file to be saved. Defaults to "pickled".
+
+    Returns:
+        str: The path to the saved pickle file.
+
+    Raises:
+        This function does not raise any specific exceptions. It relies on the pandas library to handle any file reading errors encountered.
+
+    Example:
+        directory = 'data/temperature/'
+        result = yearlyAverageDataframe(directory, name="air_temp_mean")
+        print(result)
+    """
     # load air temperature dataset
     df = pd.read_csv(directory_path, sep=";", skiprows=[0], on_bad_lines="skip")
 
-    # create new colum with timestamp from the columns year and month
+    # create new column with timestamp from the columns year and month
     df["date"] = pd.to_datetime(
         df["Jahr"].astype(str) + "-" + df["Monat"].astype(str) + "-1"
     )
@@ -62,7 +80,7 @@ def yearlyAverageDataframe(directory_path):
     # Sort the dataframe by date
     df_sorted = df.sort_values(by="date")
 
-    # get list of all states exept date
+    # get list of all states except date
     columns = df.drop("date", axis=1).columns.tolist()
 
     # groupby yearly average
@@ -70,13 +88,10 @@ def yearlyAverageDataframe(directory_path):
         df.groupby(df["date"].dt.year)[columns].mean().reset_index(drop=False)
     )
 
-    # subset of last 10 years
-    # yearly_average_last_ten_years = yearly_average.iloc[-10:]
-
     # transform the data from wide to long format
     data = yearly_average.melt("date", var_name="Bundesland", value_name="value")
 
     # save dataframe to pickle
-    processed_df = data.to_pickle(r"data\pickle\air_temp_mean.pkl")
+    processed_df = data.to_pickle(rf"data\pickle\{name}.pkl")
 
     return processed_df
--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -4,13 +4,34 @@ sys.path.append("c:/Users/paulh/Desktop/Weather")
 
 
 #directory_path_txt = r"C:\Users\paulh\Desktop\Weather\data\air_temp_mean"
-directory_path_txt = r".\.\data\air_temp_mean"
+directory_path_txt_1 = r".\.\data\air_temp_mean"
+directory_path_txt_2 = r".\.\data\precipitation_mean"
+directory_path_txt_3 = r".\.\data\sunshine_duration"
 
+name_1 = "merged_air_temp_mean.txt"
+name_2 = "merged_precipitation_mean.txt"
+name_3 = "merged_sunshine_duration.txt"
 
-func.merge_textfiles(directory_path_txt, "merged")
+path_list = [directory_path_txt_1,directory_path_txt_2,directory_path_txt_3]
+
+name_list = [name_1,name_2,name_3]
+
+for path, name in zip(path_list, name_list):
+    func.merge_textfiles(path, name)
 
 
 #directory_path = r"C:\Users\paulh\Desktop\Weather\data\merged\merged_air_temp_mean.txt"
-directory_path = r".\.\data\merged\merged_air_temp_mean.txt"
+directory_path_1 = r".\.\data\merged\merged_air_temp_mean.txt"
+directory_path_2 = r".\.\data\merged\merged_precipitation_mean.txt"
+directory_path_3 = r".\.\data\merged\merged_sunshine_duration.txt"
 
-func.yearlyAverageDataframe(directory_path)
+path_list_2 = [directory_path_1,directory_path_2,directory_path_3]
+
+name_1 = "air_temp_mean"
+name_2 = "precipitation_mean"
+name_3 = "sunshine_duration"
+
+name_list_2 = [name_1,name_2,name_3]
+
+for path,name in zip(path_list_2,name_list_2):
+    func.yearlyAverageDataframe(path, name)
